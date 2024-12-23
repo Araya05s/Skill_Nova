f92af2b3b34da4d7c4ef687355576a50b57b92bf
Dandy Arya Akbar <dandyarya003@gmail.com>
2024-12-23 14:46:20 +0700

    CRUD feature for challenges successfully created

--- skill_nova_app/lib/database/skillnova_database.dart
+++ skill_nova_app/lib/database/skillnova_database.dart
@@ -1,5 +1,8 @@
+// Singleton database class for SkillNova
+
 import 'package:flutter/foundation.dart' show immutable;
 import 'package:path/path.dart';
+import 'package:skill_nova_app/models/challenge.dart';
 import 'package:skill_nova_app/models/course_category.dart';
 import 'package:skill_nova_app/models/mission.dart';
 import 'package:sqflite/sqflite.dart';
@@ -65,6 +68,21 @@ class SkillNovaDatabase {
         FOREIGN KEY (${MissionFields.categoryId}) REFERENCES $courseCategoriesTable(${CourseCategoryFields.id}) ON DELETE CASCADE ON UPDATE CASCADE
       )
       ''');
+
+    await db.execute('''
+      CREATE TABLE IF NOT EXISTS $challengesTable (
+        ${ChallengeFields.id} $idType,
+        ${ChallengeFields.categoryId} $intType,
+        ${ChallengeFields.title} $textType,
+        ${ChallengeFields.questions} TEXT,
+        ${ChallengeFields.difficulty} $textType,
+        ${ChallengeFields.xpReward} $intType,
+        ${ChallengeFields.duration} $textType,
+        ${ChallengeFields.image} $textType,
+        ${ChallengeFields.isActive} $intType,
+        FOREIGN KEY (${ChallengeFields.categoryId}) REFERENCES $courseCategoriesTable(${CourseCategoryFields.id}) ON DELETE CASCADE ON UPDATE CASCADE
+      )
+      ''');
   }
 
   Future<CourseCategory> createCourseCategory(CourseCategory courseCategory) async {
@@ -77,6 +95,15 @@ class SkillNovaDatabase {
     final db = await instance.database;
     final id = await db.insert(missionsTable, mission.toMap());
     return mission.copy(id: id);
+  }
+
+  Future<Challenge> createChallenge(Challenge challenge) async {
+    final db = await instance.database;
+    final id = await db.insert(
+      challengesTable, 
+      challenge.toJSON()
+    );
+    return challenge.copy(id: id);
   }
 
   Future<CourseCategory> readCourseCategory(int id) async {
@@ -109,6 +136,21 @@ class SkillNovaDatabase {
     }
   }
 
+  Future<Challenge> readChallenge(int id) async {
+    final db = await instance.database;
+    final maps = await db.query(
+      challengesTable,
+      columns: ChallengeFields.values,
+      where: '${ChallengeFields.id} = ?',
+      whereArgs: [id],
+    );
+    if (maps.isNotEmpty) {
+      return Challenge.fromJSON(maps.first);
+    } else {
+      throw Exception('ID $id not found');
+    }
+  }
+
   Future<List<CourseCategory>> readAllCourseCategories() async {
     final db = await instance.database;
     const orderBy = '${CourseCategoryFields.title} ASC';
@@ -129,6 +171,15 @@ class SkillNovaDatabase {
     return result.map((missionData) => Mission.fromMap(missionData)).toList();
   }
 
+  Future<List<Challenge>> readAllChallenges() async {
+    final db = await instance.database;
+    const orderBy = '${ChallengeFields.id} DESC';
+    final result = await db.query(
+      challengesTable,
+      orderBy: orderBy
+    );
+    return result.map((challengeData) => Challenge.fromJSON(challengeData)).toList();
+  }
 
   Future<int> updateCourseCategory(CourseCategory courseCategory) async {
     final db = await instance.database;
@@ -150,6 +201,16 @@ class SkillNovaDatabase {
     );
   }
 
+  Future<int> updateChallenge(Challenge challenge) async {
+    final db = await instance.database;
+    return await db.update(
+      challengesTable,
+      challenge.toJSON(),
+      where: '${ChallengeFields.id} = ?',
+      whereArgs: [challenge.id],
+    );
+  }
+
   Future<int> deleteCourseCategory(int id) async {
     final db = await instance.database;
 
@@ -166,6 +227,16 @@ class SkillNovaDatabase {
     return await db.delete(
       missionsTable,
       where: '${MissionFields.id} = ?',
+      whereArgs: [id],
+    );
+  }
+
+  Future<int> deleteChallenge(int id) async {
+    final db = await instance.database;
+
+    return await db.delete(
+      challengesTable,
+      where: '${ChallengeFields.id} = ?',
       whereArgs: [id],
     );
   }
